2aeb789381598818887ceeb818adc0ba788e1c0e
visserid <74188981+visserid@users.noreply.github.com>
2021-11-17 18:48:00 +0100

    JavascriptBasics

---
+++ JavascriptBasics/script.js
@@ -0,0 +1,24 @@
+
+console.log('Hello Winc Academy');
+
+let name = 'Ineke';
+console.log(name);
+"4" + "4"
+'44'
+4+4
+8
+let y = 20;
+let x = y + 3;
+console.log(x)
+let x = y- 3;
+console.log(x);
+let x = y * 33;
+console.log(x);
+let x = y / 0.5;
+console.log(x);
+let x = y % 10;
+console.log(x);
+let age = 33;
+console.log (typeof age);
+let age = "33";
+console.log (typeof age);
