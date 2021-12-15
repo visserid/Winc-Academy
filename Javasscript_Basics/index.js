8f1b6d407c883db6e9770361393336ec59bb62eb
visserid <74188981+visserid@users.noreply.github.com>
2021-12-15 19:28:26 +0100

    Add files via upload

---
+++ Javasscript_Basics/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Winc Academy students")
+
+// This is a large sum
+
+
+/* let sum = 1230941 * 1823794
+console.log("sum:", sum);
+
+let division = 637263 / 54
+console.log("division:", division); */
+
+// Because the calculations are now in a comment
