2f39d41013059abdea25789b658f60acafe27008
visserid <74188981+visserid@users.noreply.github.com>
2021-12-18 21:31:35 +0100

    if_else

---
+++ Javascript_if_else/pub.js
@@ -0,0 +1,35 @@
+ 
+{ const age = 20;
+
+   if (age >= 18){
+
+    console.log("Welcome to the pub!");
+
+    } else if (age <= 18){
+
+    console.log("You are too young!");
+
+    } 
+}    
+
+{
+    const isFemale = true;
+
+    if (isFemale) {
+
+        console.log("Welcome girl!");
+
+    } else {
+       console.log("It's ladies night, no boys allowed!");
+    }
+} 
+
+{
+    const driverStatus = 'bob';
+
+    if (driverStatus) {
+        console.log("You can drive!");
+    } else {
+        console.log("You can't drive");
+    }
+}
