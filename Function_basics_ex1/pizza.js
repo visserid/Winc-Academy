2b34b5d3f3dff3f0a3d2bced8b2076a3e64796d6
visserid <74188981+visserid@users.noreply.github.com>
2021-12-19 10:30:48 +0100

    Add files via upload

---
+++ Function_basics_ex1/pizza.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log('Make dough');
+    console.log('Make tomato sauce');
+    console.log('Put sauce on pizza');
+    console.log('Add cheese on pizza ');
+    console.log('Put pizza in the oven');
+};
+
+makePizza();
+
+const makeSushi = function () {
+    console.log('cook rice');
+    console.log('cut veggies');
+    console.log('roll sushi');
+    console.log('cut sushiroll in pieces');    
+};
+
+makeSushi();
+makeSushi();
+makePizza();
+makeSushi();
+makePizza();
