1c6c681b8a4fd5d5ab14b2e9469816ae126e0db6
visserid <74188981+visserid@users.noreply.github.com>
2021-12-19 21:17:20 +0100

    Add files via upload

---
+++ loops/even-odd-reporter.js
@@ -0,0 +1,17 @@
+
+
+for (let i = 0; i <= 20; i++) {
+
+    if ( i === 0) {
+
+    console.log ( i + ' is even');
+
+    } else if ( i % 2 === 0){
+    
+    console.log (i + ' is even');
+
+    } else {
+    console.log (i + ' is odd');
+    }
+};
+
---
+++ loops/multiplication-tables.js
@@ -0,0 +1,10 @@
+
+for (let i = 1 ; i <= 10; i++ ) {
+ 
+    for (let number = 1 ; number <= 10; number++){
+ 
+    const result = i * number;
+   
+    console.log(i + ' * ' + number + ' = ' + result);
+}
+};
